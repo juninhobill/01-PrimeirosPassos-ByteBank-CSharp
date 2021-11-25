bb98462e0501423a799a342d10ddab406261a6e6
juninhobill <billv6@gmail.com>
2021-11-25 13:43:43 -0300

    Add project files.

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!!! Meu primeiro programa!!!");
+
+            Console.WriteLine("A execução terminou, tecle enter para finalizar...");
+
+                Console.ReadLine();
+        }
+    }
+}
